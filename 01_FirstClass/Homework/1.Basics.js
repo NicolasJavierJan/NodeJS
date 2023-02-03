e6a02bd3f6fc7d0d18c8c5eb624be4424c5d400d
NicolasJavierJan <nicolasjavierjan@gmail.com>
2023-02-03 11:32:48 +0100

    Changed some files and created new folders

--- 01_FirstClass/Homework/1.Basics.js
+++ 01_FirstClass/Homework/1.Basics.js
@@ -9,7 +9,10 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+// String concatenation approach
 console.log("My first name is " + firstName + " and my last name is " + lastName);
+// String interpolation approach
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -18,11 +21,11 @@ const year = "2021";
 const number = 1;
 
 // Add the year plus the number
-// The result should be 2020
+// The result should be 2022
 // You cannot touch line 1 or 2
 
-console.log(year - number)
-console.log(parseInt(year) - number)
-
-
+console.log(parseInt(year) + number);
+console.log(Number(year) + number);
+// Writing + in front of a String converts it to Number.
+console.log(+year + number);
 // --------------------------------------
