12230aebdadc43b6aea36d32bcf0d7c695033040
NicolasJavierJan <nicolasjavierjan@gmail.com>
2023-04-22 17:05:54 +0200

    added client: main page

---
+++ Mandatory02/client/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Router, Route, Link } from "svelte-navigator";
+  import Home from "./pages/home/Home.svelte";
+  import Login from "./pages/login/Login.svelte";
+  
+</script>
+
+<Router>
+  <nav class="nav-bar">
+    <Link to="/">Home</Link>
+    <Link to="/login">Login!</Link>
+  </nav>
+  <div class="main-page">
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/login">
+      <Login />
+    </Route>
+  </div>
+</Router>
+
+
---
+++ Mandatory02/client/src/pages/home/Home.svelte
@@ -0,0 +1,3 @@
+<h1>Mandatory II</h1>
+<h2>For Full Stack Development with NodeJS</h2>
+<h3>By Nicolas Javier Jan</h3>
